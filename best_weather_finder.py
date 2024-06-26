ab1aadf4eb5be39c1e19123c39ab5a53f05dff8c
LLKE <laurens.keuker@gmail.com>
2024-06-26 08:29:38 +0200

    Added map, some input parsing, some renaming, creating a green marker where the weather is best

--- best_weather_finder.py
+++ best_weather_finder.py
@@ -2,7 +2,9 @@ import requests
 import folium
 from geopy.distance import geodesic
 from datetime import datetime, timezone
+from streamlit_folium import folium_static
 import streamlit as st
+from typing import Optional, Tuple
 
 # Function to get towns within a radius
 def get_towns_within_radius(center_lat, center_lon, radius_km, min_population=500):
@@ -10,6 +12,7 @@ def get_towns_within_radius(center_lat, center_lon, radius_km, min_population=50
     overpass_query = f"""
     [out:json];
     (
+      node["place"="city"](around:{radius_km * 1000},{center_lat},{center_lon})["population"];
       node["place"="town"](around:{radius_km * 1000},{center_lat},{center_lon})["population"];
       node["place"="village"](around:{radius_km * 1000},{center_lat},{center_lon})["population"];
     );
@@ -29,18 +32,20 @@ def get_towns_within_radius(center_lat, center_lon, radius_km, min_population=50
                 
     return towns
 
+
 # Function to get weather data for multiple towns
 def get_weather_data_for_towns(towns, api_key):
-    weather_data = []
+    weather_data_list = []
     for town in towns:
         lat, lon = town[1], town[2]
         url = f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api_key}&units=metric"
         response = requests.get(url).json()
-        weather_data.append((town[0], response))
-    return weather_data
+        weather_data_list.append((town[0], response))
+    return weather_data_list
+
 
 # Function to calculate weather score
-def calculate_weather_score(weather_data, weights):
+def calculate_weather_score(weather_data, weights) -> float:
     sunrise = datetime.fromtimestamp(weather_data['city']['sunrise'], tz=timezone.utc)
     sunset = datetime.fromtimestamp(weather_data['city']['sunset'], tz=timezone.utc)
 
@@ -80,6 +85,7 @@ def calculate_weather_score(weather_data, weights):
 
     return total_score
 
+
 # Function to calculate value based on temp, wind, rain
 def calculate_value(temp, wind_speed, rain):
     if temp > 25:
@@ -115,7 +121,7 @@ def select_homonymous_locations(locations):
     return selected_option
         
 
-def get_user_coordinates(location):
+def get_user_coordinates(location) -> Optional[Tuple[float, float]]:
     overpass_url = "http://overpass-api.de/api/interpreter"
     overpass_query = f"""
     [out:json];
@@ -138,13 +144,49 @@ def get_user_coordinates(location):
         return None, None
 
 
+def calculate_weather_scores_and_max(weather_data_list, weights) -> Tuple[list, str]:
+    weather_scores = []
+    max_score_town = None
+    max_score = 0
+    for town, weather in weather_data_list:
+        score = calculate_weather_score(weather, weights)
+        if score > max_score:
+            max_score = score
+            max_score_town = town
+        weather_scores.append((town, weather, score))
+
+    return weather_scores, max_score_town
+
+
+def display_on_map(weather_scores, max_score_town, center_lat, center_lon):
+        # Visualize on a map
+    map_center = [center_lat, center_lon]
+    mymap = folium.Map(location=map_center, zoom_start=10)
+
+    for weather_score in weather_scores:
+        if weather_score[0] == max_score_town:
+            folium.Marker(
+                location=[weather_score[1]['city']['coord']['lat'], weather_score[1]['city']['coord']['lon']],
+                popup=f"{weather_score[0]}: {weather_score[2]:.2f}",
+                icon=folium.Icon(color='green')
+            ).add_to(mymap)
+        else:
+            folium.Marker(
+                location=[weather_score[1]['city']['coord']['lat'], weather_score[1]['city']['coord']['lon']],
+                popup=f"{weather_score[0]}: {weather_score[2]:.2f}"
+            ).add_to(mymap)
+
+    return mymap
+
+
 def find_best_weather(user_location, user_radius, user_population):
     #! Insert your OpenWeatherMap API key here. Create account, activate via email link, API key will be emailed to you.
     api_key = '88d3e456f87cc1e050630459f9c1a301'
     center_lat, center_lon = get_user_coordinates(user_location)  # Example coordinates for Aachen, Germany
     
+    weather_map = None
     if center_lat is None or center_lon is None:
-        return False
+        return False, weather_map
     else:
         coordinates_found = True
     
@@ -152,45 +194,29 @@ def find_best_weather(user_location, user_radius, user_population):
 
     towns = get_towns_within_radius(center_lat, center_lon, user_radius, user_population)
     weather_data_list = get_weather_data_for_towns(towns, api_key)
+    weather_scores, max_score_town = calculate_weather_scores_and_max(weather_data_list, weights)
+    weather_map = display_on_map(weather_scores, max_score_town, center_lat, center_lon)
 
-    # Calculate and print weather scores for each town
-    for town_name, weather_data in weather_data_list:
-        score = calculate_weather_score(weather_data, weights)
-        print(f"Total weather score for {town_name}: {score}")
+    return coordinates_found, weather_map
 
-    # Visualize on a map
-    map_center = [center_lat, center_lon]
-    mymap = folium.Map(location=map_center, zoom_start=10)
+if __name__ == "__main__":
+    st.title('Best Weather Finder')
+    st.subheader('Your solution to summer, wherever and whenever!')
+    user_location = st.text_input('Where are you traveling from? Make sure to spell it correctly and in English!')
+    user_location = user_location.lower().capitalize()
+    user_radius = st.slider('How far are you willing to travel?', 0, 100, 5)
+    user_population = st.slider('Minimum population of town?', 0, 1000000, 500)
+    if st.button('Find Best Weather!'):
+        st.write('Finding the best weather...')
+        st.write('This may take a few seconds...')
+        coordinates_found, weather_map = find_best_weather(user_location, user_radius, user_population)
+        if not coordinates_found:
+            st.write('Could not find coordinates for user location. Exiting...')
+            exit()
+        elif coordinates_found:
+            st.write('Found coordinates for user location!')
 
-    for town_name, weather_data in weather_data_list:
-        score = calculate_weather_score(weather_data, weights)
-        folium.Marker(
-            location=[weather_data['city']['coord']['lat'], weather_data['city']['coord']['lon']],
-            popup=f"{town_name}: {score:.2f}"
-        ).add_to(mymap)
-
-    mymap.save('weather_scores_map.html')
-    print('Map saved as weather_scores_map.html')
-
-    return coordinates_found
-
-# Main code 
-
-st.title('Best Weather Finder')
-st.subheader('Your solution to summer, wherever and whenever!')
-user_location = st.text_input('Where are you traveling from?')
-user_radius = st.slider('How far are you willing to travel?', 0, 100, 5)
-user_population = st.slider('Minimum population of town?', 0, 1000000, 500)
-if st.button('Find Best Weather!'):
-    st.write('Finding the best weather...')
-    st.write('This may take a few seconds...')
-    coordinates_found = find_best_weather(user_location, user_radius, user_population)
-    if not coordinates_found:
-        st.write('Could not find coordinates for user location. Exiting...')
-    elif coordinates_found:
-        st.write('Found coordinates for user location!')
-
-    
+        folium_static(weather_map)
 
 # TODO 1: Add a function to get the best town based on weather score
 # TODO 2: Display weather scores on the map and color code the markers based on the score
@@ -200,3 +226,5 @@ if st.button('Find Best Weather!'):
 # TODO 6: Optimize the code for performance and readability
 # TODO 7: Fine tune cost function
 # TODO 8: Add Time of day you want to travel
+# TODO 9: Parse input location to be case insensitive
+# TODO 10: Add return and parameter types to functions
